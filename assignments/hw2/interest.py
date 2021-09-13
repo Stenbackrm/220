c361a466a88a738c7b7227dafbf1b78fcd524ee9
Mythicshade <57931570+Mythicshade@users.noreply.github.com>
2021-09-13 09:33:34 -0400

    hw 2 finnished

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -2,6 +2,12 @@
 This program calculates the annual interest rate given the proper information
 '''
 
+#collect input from the user
+#convert input into forms used in calculation
+#use days in billing cylcle and the payment date to find how many days left
+#find average daily balance using the interest formula
+#use this to find the montly interest charge
+
 def main():
     annual_interest = eval(input('what is the annual interest rate? please enter:'))
     annual_interest_decimal = annual_interest /100
