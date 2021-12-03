955492a2f3549f51b4d09a7647d5f6ad44228954
Mythicshade <57931570+Mythicshade@users.noreply.github.com>
2021-12-03 01:28:45 -0500

    fixed and finished issues with program

--- assignments/hw10/sales_force.py
+++ assignments/hw10/sales_force.py
@@ -41,21 +41,40 @@ class SalesForce:
     def top_seller(self):
         sales_dict = {}
         sales_totals = []
+        ranking = []
         for i in self.sales_people:
-            total, person = i.total_sales, i
+            total, person = i.total_sales(), i
             sales_dict[total] = person
-        for i in sales_dict:
-            sales_totals.append(i)
-        print(sales_totals)
+        for i in self.sales_people:
+            sales_totals.append(i.total_sales())
+        selection_sort(sales_totals)
+        for i in range(len(sales_totals)):
+            ranking.append(sales_dict[sales_totals[i]])
+        rank_index = []
+        if sales_totals[-1] == sales_totals[-2]:
+            for i in range(len(sales_totals) - 1):
+                if sales_totals[-1] == sales_totals[i]:
+                    rank_index.append(i)
+            selection_sort(rank_index)
+            return [ranking[rank_index[0]:]]
+        return [ranking[-1]]
 
-
+    def individual_sales(self, employee_id):
+        id_dict = {}
+        for i in self.sales_people:
+            list_id, person = i.get_id(), i
+            id_dict[list_id] = person
+        try:
+            return id_dict[employee_id]
+        except KeyError:
+            return None
 
 
 def main():
     walmart = SalesForce()
     walmart.add_data("salesData.txt")
     print(walmart.quota_report(100))
-    walmart.top_seller()
+    print(walmart.top_seller())
+    print(walmart.individual_sales(345))
 if __name__ == '__main__':
     main()
-
